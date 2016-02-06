50c82769c6c48fbbcc02fb46ee91c62104b8d92b
hang <git@hang.im>
2016-02-06 15:29:35 +0800

    Regularized Linear Regression Cost Function

--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -18,8 +18,8 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
-
-
+H = X * theta;
+J = (sum((H - y) .^ 2) + lambda * sum(theta(2:end) .^ 2)) / (2 * m);
 
 
 
